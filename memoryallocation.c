64f04c14c7571ae750d41e69af824ffb9912d9ab
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2023-03-27 11:27:32 +0530

    Add files via upload

---
+++ memoryallocation.c
@@ -0,0 +1,81 @@
+// memory allocate ,even odd, + - ,reverse, sort
+
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	int n, *p, i;
+	printf("Enter size : ");
+	scanf("%i",&n);
+	p= (int *)malloc(n*sizeof(int ));
+	for(i=0;i<n;i++)
+	{
+		scanf("%i",p+i);
+	}
+	
+	evenodd(p, n);
+	posneg(p, n);
+	rev(p, n);
+	sort(p ,n);
+}
+void evenodd(int *p, int n)
+{
+	int i,even=0,odd=0;
+	for(i=0;i<n;i++)
+	{
+		if (*(p+i)%2==0)
+		{
+			even++;
+		}
+		else
+		{
+			odd++;
+		}
+	}
+	printf("Even elements are : %i \n",even);
+	printf("Odd elements are : %i",odd);
+}
+void posneg(int *p, int n)
+{
+	int i,pos=0,neg=0;
+	for(i=0;i<n;i++)
+	{
+		if (*(p+i)>0)
+		{
+			pos++;
+		}
+		else if (*(p+i)<0)
+		{
+			neg++;
+		}
+	}
+	printf("Positive elements are : %i \n",pos);
+	printf("Negative elements are : %i",neg);
+}
+void rev(int *p, int n)
+{
+	for (int i=0;i<n;i++)
+	{
+	printf("%i",*(p+n-i-1));
+	}
+}
+void sort(int *p, int n)
+{	
+	int i,j,t;
+	for (i=0;i<n;i++)
+	{	
+		for (j=i+1;j<n;j++)
+		{
+			if(*(p+i)>*(p+j))
+			{
+				t=*(p+i);
+				*(p+i)=*(p+j);
+				*(p+j)=t;
+			}
+		}
+	}
+	for (i=0;i<n;i++)
+	{
+		printf("%i",*(p+i));
+	}
+}
