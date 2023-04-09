5004c278358c2bdeb5c329eaf1c0d34757246ac8
Divyansh Dhaundiyal <112102507+Divyansh1316@users.noreply.github.com>
2023-04-09 20:31:32 +0530

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,36 +1,38 @@
 /* 5. WAP to input a string="Aman singh rawat" and print ASRawat as output. */
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 void main()
 {
-	int i,l,j,flag;
-	char str[20]="Aman Singh Rawat",rev[20];
-	l=strlen(str);
-	if (str[0]!=' ')
-	{
-		flag=1;
-		printf("%c",str[0]);
-	}
-	for (i=0;i<l;i++)
-	{
-		if (str[i]!='\0')
-		{
-			if (str[i]==' ' && str[i+1]!=' ')
-			{
-				flag++;
-			
-			if (flag>2)
-			{
-				break;}
-				printf("%c",str[i+1]);
-			
-			}
-		}
-	}
-	for (j=i+1;j<l;j++)
-	{
-		printf("%c",str[j]);
-	}
-	
+    int i, l, j, flag;
+    char str[50], rev[50];
+    printf("Enter a string : ");
+    gets(str);
+    printf("\n");
+    l = strlen(str);
+    if (str[0] != ' ')
+    {
+        flag = 1;
+        printf("%c", str[0]);
+    }
+    for (i = 0; i < l; i++)
+    {
+        if (str[i] != '\0')
+        {
+            if (str[i] == ' ' && str[i + 1] != ' ')
+            {
+                flag++;
+
+                if (flag > 2)
+                {
+                    break;
+                }
+                printf("%c", str[i + 1]);
+            }
+        }
+    }
+    for (j = i + 1; j < l; j++)
+    {
+        printf("%c", str[j]);
+    }
 }
